d3ca3c11e830beee8ebe012f741cc71872fc0f3b
Jesus García <jeigar2@gmail.com>
2022-10-26 03:04:47 +0200

    Agregado product detail + click carrito compra

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const elementMenuClass = ".navbar-email";
-const elementMenu = document.querySelector(elementMenuClass);
+const elementClassMenu = ".navbar-email";
+const elementMenu = document.querySelector(elementClassMenu);
 const desktopMenu = document.querySelector(".desktop-menu");
 
 elementMenu.addEventListener("click", toggleDesktopMenu);
@@ -15,5 +15,21 @@ const mobileMenu = document.querySelector(".mobile-menu");
 elementMenuMovile.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
+    const isProductDetailClose = productDetail.classList.contains("inactive");
+    if(!isProductDetailClose)
+        toggleProductDetail();
     mobileMenu.classList.toggle("inactive");
+}
+
+const elementMenuClassShoppingCart = ".navbar-shopping-cart";
+const elementMenuShoppingCart = document.querySelector(elementMenuClassShoppingCart);
+const productDetail = document.querySelector(".product-detail");
+
+elementMenuShoppingCart.addEventListener("click", toggleProductDetail);
+
+function toggleProductDetail() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    if(!isMobileMenuClosed)
+        toggleMobileMenu();
+    productDetail.classList.toggle("inactive");
 }
